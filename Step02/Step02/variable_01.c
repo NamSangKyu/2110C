eea569501153116b9767d2c5aee393c8a07ae542
NamSangKyu <nam2626@gmail.,com>
2021-10-14 19:53:09 +0900

    변수

---
+++ Step02/Step02/variable_01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+*   변수 - variable
+        데이터를 하나 저장할 공간
+*/
+int main(void) {
+    int box;//정수를 하나 저장할 공간을 설정, 이름 box을 정함
+    box = 10;
+    printf("%d\n", box);
+    printf("%d\n", box*2);
+
+    return 0;
+}
