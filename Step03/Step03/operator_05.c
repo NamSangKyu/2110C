d64bb2386f447c24185492a88a121a181d47de74
NamSangKyu <nam2626@gmail.,com>
2021-10-19 21:38:46 +0900

    논리 연산자

--- Step03/Step03/operator_05.c
+++ Step03/Step03/operator_05.c
@@ -11,7 +11,7 @@ int main(void) {
     *       AND  :   &&    ---> 양쪽의 조건이 모두 true일떄 true
     *       OR   :   ||    ---> 양쪽의 조건이 하나라도 true면 true
     *       NOT  :   !     ---> true면 false, false면 true 뒤집는 연산자
-    */
+    */ 
     printf("%d\n", a > 5 && a < 10);
     printf("%d\n", a < 5 || a > 7);
     printf("%d\n", !(a > 5));
