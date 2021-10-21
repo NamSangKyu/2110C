3a628c9a7c51362b23b53d8da113b7c92da685a4
NamSangKyu <nam2626@gmail.,com>
2021-10-21 19:34:16 +0900

    scanf 예제

--- Step04/Step04/input_double.c
+++ Step04/Step04/input_double.c
@@ -6,7 +6,15 @@ int main(void) {
         실수 하나 입력을 받아서
         해당 실수의 원넓이를 출력
     */
+    double r, area;
+    const double PI = 3.1415;//상수 - 바뀌지 않는 값
+    
+    printf("반지름 하나 입력 : ");
+    scanf("%lf", &r);
 
+    area = r * r * PI;
+
+    printf("원넓이 : %g\n", area);
 
     return 0;
 }
