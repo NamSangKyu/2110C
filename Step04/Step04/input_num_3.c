a8b0ecfce568ed34b1ac698036ba05b642a6e746
NamSangKyu <nam2626@gmail.,com>
2021-10-21 20:02:56 +0900

    scanf 예제

---
+++ Step04/Step04/input_num_3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c;
+    int result;
+    printf("숫자 3개 입력 :");
+    scanf("%d %d %d", &a, &b, &c);
+    result = (a - b) * (b + c) / (a * c);
+    printf("result : %d\n", result);
+
+    return 0;
+}
