e0101d38ddf7fcab0e5e7ca5d72e36391f777cda
NamSangKyu <nam2626@gmail.,com>
2021-10-22 20:17:21 +0900

    반복문 중첩

--- Step05/Step05/loop_in_loop_q2.c
+++ Step05/Step05/loop_in_loop_q2.c
@@ -5,18 +5,36 @@
     입력받은 정수만큼 삼각형을 출력
 
     숫자 입력 : 7
-                줄번호     별개수
-          *     1           1
-         **     2           2
-        ***     3           3
-       ****     4           4
-      *****     5           5
-     ******     6           6
-    *******     7           7
+                줄번호     별개수    공백 개수
+          *     1           1       6
+         **     2           2       5
+        ***     3           3       4
+       ****     4           4       3
+      *****     5           5       2
+     ******     6           6       1
+    *******     7           7       0
 
 */
 int main(void) {
-    
+    //    줄개수 별개수 공백개수
+    int i=0, line, star, space;
 
+    printf("줄개수 입력 : ");
+    scanf_s("%d", &line);
+
+    while (i < line) {
+        space = 0;
+        while (space < line - 1 - i) {
+            printf(" ");
+            space++;
+        }
+        star = 0;
+        while (star <= i) {
+            printf("*");
+            star++;
+        }
+        printf("\n");
+        i++;
+    }
     return 0;
 }
