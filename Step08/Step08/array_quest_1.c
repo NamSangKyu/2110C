bb8817b1c60c2db1ca8b0f0505cd3b12dd239470
NamSangKyu <nam2626@gmail.,com>
2021-11-02 19:45:35 +0900

    배열  문제

--- Step08/Step08/array_quest_1.c
+++ Step08/Step08/array_quest_1.c
@@ -3,17 +3,24 @@
     정수형 배열 길이가 10개짜리 선언 후
     배열에 내용을 직접 입력받는 반복문을 작성
 
-
+    1. 배열에 입력된 숫자들의 총합을 출력
 */
 int main(void) {
     int arr[10];
-    int i;
-    int len = sizeof(arr) / sizeof(int);
+    int i, total;
+    const int len = sizeof(arr) / sizeof(int);
     for (i = 0; i < len; i++) {
         printf("%d 번째 숫자 입력 : ", i + 1);
         scanf_s("%d", &arr[i]);
     }
-
-
+    //----------------------------------
+    total = 0;
+    for (i = 0; i < len; i++) {
+        total += arr[i];
+    }
+    printf("총합 : %d\n", total);
     return 0;
 }
+
+
+
