b2f8e71d43059a5578fa32ca56ea317d7eae7dbf
NamSangKyu <nam2626@gmail.,com>
2021-11-02 20:15:37 +0900

    배열  문제

--- Step08/Step08/array_quest_1.c
+++ Step08/Step08/array_quest_1.c
@@ -4,10 +4,12 @@
     배열에 내용을 직접 입력받는 반복문을 작성
 
     1. 배열에 입력된 숫자들의 총합을 출력
+    2. 배열에 입력된 숫자들 중 제일 큰 숫자를 출력
+    3. 배열에 입력된 숫자들 중 제일 작은 숫자를 출력
 */
 int main(void) {
     int arr[10];
-    int i, total;
+    int i, total, max;
     const int len = sizeof(arr) / sizeof(int);
     for (i = 0; i < len; i++) {
         printf("%d 번째 숫자 입력 : ", i + 1);
@@ -15,10 +17,14 @@ int main(void) {
     }
     //----------------------------------
     total = 0;
+    max = arr[0];
     for (i = 0; i < len; i++) {
         total += arr[i];
+        if (max < arr[i])
+            max = arr[i];
     }
     printf("총합 : %d\n", total);
+    printf("최대값 : %d\n", max);
     return 0;
 }
 
