686a600ccf47b5a58a02f5b1c6c0e6a3cf46f710
NamSangKyu <nam2626@gmail.,com>
2021-11-09 20:10:07 +0900

    재귀 함수

---
+++ Step11/Step11/recursive_quest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//매개변수로 숫자를 하나 받아서 해당 숫자의 팩토리얼 값을 구하는 함수
+//5 -> 1 * 2 * 3 * 4 * 5 --> 120
+int fac(int n) {
+    if (n == 1)
+        return 1;
+    return n * fac(n - 1);
+}
+
+int main(void) {
+    printf("%d! = %d\n",5, fac(5));
+    printf("%d! = %d\n",6, fac(6));
+    printf("%d! = %d\n",7, fac(7));
+    return 0;
+}
