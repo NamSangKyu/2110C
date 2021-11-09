cea3649264553bd5a4f66628949f467db89117fe
NamSangKyu <nam2626@gmail.,com>
2021-11-09 21:14:36 +0900

    2차원 배열 문제

--- Step11/Step11/2DArray_quest_1.c
+++ Step11/Step11/2DArray_quest_1.c
@@ -8,6 +8,17 @@
     4  8 12 16 20 24 28 32 36
 */
 int main(void) {
+    int arr[3][9];
+    int i, j;
 
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 9; j++)
+            arr[i][j] = (i + 2) * (j + 1);
+    }
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 9; j++)
+            printf("%2d ", arr[i][j]);
+        printf("\n");
+    }
     return 0;
 }
