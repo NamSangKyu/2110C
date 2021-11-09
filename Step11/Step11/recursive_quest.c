818f9e8da477a42bdbd945dea906b177294c1bf1
NamSangKyu <nam2626@gmail.,com>
2021-11-09 20:10:37 +0900

    재귀 함수

--- Step11/Step11/recursive_quest.c
+++ Step11/Step11/recursive_quest.c
@@ -6,7 +6,12 @@ int fac(int n) {
         return 1;
     return n * fac(n - 1);
 }
-
+//return 5 * [return 4 * [return 3 * [return 2 * [return 1]]]]
+//return 5 * [return 4 * [return 3 * [return 2 * 1]]]
+//return 5 * [return 4 * [return 3 * 2]]
+//return 5 * [return 4 * 6]
+//return 5 * 24
+//return 120
 int main(void) {
     printf("%d! = %d\n",5, fac(5));
     printf("%d! = %d\n",6, fac(6));
