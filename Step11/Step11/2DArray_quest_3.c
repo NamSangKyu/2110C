ed7b5430998c16e65175b66b3224d4c9a3bcbace
NamSangKyu <nam2626@gmail.,com>
2021-11-11 19:36:33 +0900

    문제 풀이

--- Step11/Step11/2DArray_quest_3.c
+++ Step11/Step11/2DArray_quest_3.c
@@ -10,7 +10,26 @@
 */
 int main(void) {
     int arr[5][5];
-    int i, j, n;
+    int i, j=0, n = 1, sw=1;
+
+    for (i = 0; i < 5; i++) {
+ 
+        while (!(j == -1 || j == 5)) {
+            arr[i][j] = n++;
+            j += sw;
+        }
+        if (sw == 1)
+            sw = -1;
+        else
+            sw = 1;
+        j += sw;
+    }
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            printf("%2d ", arr[i][j]);
+        }
+        printf("\n");
+    }
 
 
     return 0;
