e55ca167f590bc016cd805483cb9e72047e18e2e
NamSangKyu <nam2626@gmail.,com>
2021-11-23 21:37:05 +0900

    리스트

--- Step16/Step16/list.c
+++ Step16/Step16/list.c
@@ -51,8 +51,22 @@ void NodeUpdate(List* list, int search_data, int update_data) {
 }
 //전체 노드 출력
 void PrintAllNode(List* list) {
+    //ptr이 NULL 나올때까지 이동
+    Node* ptr = list->header;
+    if (list->size == 0) {
+        printf("리스트에 저장된 내용이 없습니다\n");
+        return;
+    }
+    while (ptr != NULL) {
+        //이동 할때마다 ptr->val을 출력
+        printf("%d->", ptr->val);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
 }
 int main(void) {
-
+    //1. 노드 추가
+    //5. 전체 노드 출력
+    //0. 프로그램 종료
     return 0;
 }
