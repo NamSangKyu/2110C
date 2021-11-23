c0bb38580c12aa2edb78fec548807156bb23cb6e
NamSangKyu <nam2626@gmail.,com>
2021-11-23 20:24:29 +0900

    큐

---
+++ Step16/Step16/queue.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+const int QUEUE_SIZE = 5;
+int queue[5];
+int front, rear;
+
+//큐에 저장된 내용을 전부 출력
+void PrintQueue() {
+    
+}
+//큐에 데이터를 입력 받아서 저장
+void Enque() {
+    //큐에 저장될 공간이 있는지 체크
+    //저장할 다음 위치 (rear + 1) % QUEUE_SIZE 다음 위치
+    //큐에 저장된 내용을 전부 출력
+}
+//큐에 저장된 데이터를 뽑음
+void Deque() {
+}
+int main(void) {
+    return 0;
+}
