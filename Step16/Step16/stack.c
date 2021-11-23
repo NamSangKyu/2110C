e508d42bcc5bf22ada78ea6238a43fcf77e9e1b7
NamSangKyu <nam2626@gmail.,com>
2021-11-23 19:50:55 +0900

    스택

--- Step16/Step16/stack.c
+++ Step16/Step16/stack.c
@@ -24,25 +24,50 @@ void Init() {
 int Push(int n) {
     //저장할 공간이 있는지 체크
     //  저장할 공간이 없으면 0 리턴
+    if (top == max) return 0;
     //데이터 추가
     //TOP 증가
+    stack[top++] = n;
     //데이터 추가가 정상적으로 되면 1리턴
+    return 1;
 }
 //데이터 읽기
 int Pop() {
     //저장된 데이터가 있는지 체크
     //  저장된 데이터가 없으면 -1 리턴
+    if (top == 0) return -1;
     //데이터를 리턴 TOP을 감소
+    return stack[--top];
 }
 int main(void) {
-    /*
-    *   프로그램 제어
-    *   1. push
-    *   2. pop
-    *   3. exit
-    *   input meno no : 
-    *   
-    */
+    int no;
+    Init();
+    while (1) {
+        printf("1. push\n");
+        printf("2. pop\n");
+        printf("3. exit\n");
+        printf("input meno no : ");
+        scanf_s("%d", &no);
+        if (no == 1) {
+            printf("추가할 숫자 입력 : ");
+            scanf_s("%d", &no);
+            if (Push(no))
+                printf("데이터 추가 성공\n");
+            else
+                printf("데이터 추가 실패\n");
+        }
+        else if (no == 2) {
+            no = Pop();
+            if (no == -1)
+                printf("스택에 데이터가 없습니다.\n");
+            else
+                printf("POP : %d\n", no);
+
+        }
+        else {
+            break;
+        }
+    }
 
 }
 
