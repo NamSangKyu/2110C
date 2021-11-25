afa04649e07ad0a430de3cbb331c8c70a426852d
NamSangKyu <nam2626@gmail.,com>
2021-11-25 21:26:16 +0900

    C++ 출력

---
+++ Step16/Step16/cout.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void) {
+    int n = 100;
+    double p = 3.1415;
+    char str[] = "æ»≥Á«œººø‰";
+    //                              \n
+    std::cout << "n : " << n << std::endl;
+    std::cout << "p : " << p << std::endl;
+    std::cout << "str : " << str << std::endl;
+
+    return 0;
+}
