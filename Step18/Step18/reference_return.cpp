b5758c1efdaa4444d6901fd86681b2cc5c33fc1a
NamSangKyu <nam2626@gmail.,com>
2021-11-30 21:18:40 +0900

    참조자

--- Step18/Step18/reference_return.cpp
+++ Step18/Step18/reference_return.cpp
@@ -12,6 +12,8 @@ int main(void) {
     cout << num << " " << ref << endl;
     cout << &num << " " << &ref << endl;
 
+    //매개변수 참조자, 리턴 참조, 받는 변수는 일반 변수일 경우
+    //일반 변수일때는 값을 참조자로 리턴 해도 변수가 새로 만들어지기 때문에 값만 복사받는다
     int var = add(num);
 
     cout << num << " " << var << endl;
