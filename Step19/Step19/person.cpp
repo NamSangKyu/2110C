c17e8260d59032ea3d1177bea773a2409bd42349
NamSangKyu <nam2626@gmail.,com>
2021-12-02 20:17:26 +0900

    클래스

--- Step19/Step19/person.cpp
+++ Step19/Step19/person.cpp
@@ -13,6 +13,20 @@ private:
     char name[30];
     int age;
 public:
+    //생성자 : 클래스를 이용하여 변수 선언시 제일먼저 딱 한번 실행되는 함수
+    //        클래스가 생성이 되었을때 제일 먼저 해야되는 일들을 처리하는 함수
+    // 생성자는 클래스 명이 함수명, 리턴타입이 없는 함수
+    Person() {
+        cout << "Person 생성자" << endl;
+    }
+    Person(const char* n, int a) {
+        cout << "Person(const char *, int) 생성자 " << endl;
+        strcpy_s(name, sizeof(name), n);
+        if (a >= 0)
+            age = a;
+        else
+            age = 0;
+    }
     //멤버 함수
     void Init(const char* n, int a) {
         //n은 name에 복사
@@ -31,5 +45,9 @@ int main(void) {
     p.Init("홍길동", 20);
     //p.age = -20; age가 private 영역에 있기 때문에 접근할 수 없다.
     p.PrintInfo();
+
+    Person a("김철수", 22);
+    a.PrintInfo();
+
     return 0;
 }
