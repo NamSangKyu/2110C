044b582ba555322922cfdee57000ad7b7bf0a4cb
NamSangKyu <nam2626@gmail.,com>
2021-12-02 21:42:18 +0900

    클래스

--- Step19/Step19/class_array.cpp
+++ Step19/Step19/class_array.cpp
@@ -17,6 +17,10 @@ public:
 };
 int main(void) {
     //Position arr[5];//기본 생성자가 없으면 배열선언이 안됨
-
+    Position* arr[5];//포인터변수를 배열로 선언
+    arr[0] = new Position(10, 20);//좌표값 저장하는 클래스를 동적할당해서 생성
+    arr[0]->PrintInto();
+    arr[1] = new Position(3, 5);
+    (*arr[1]).PrintInto();
     return 0;
 }
