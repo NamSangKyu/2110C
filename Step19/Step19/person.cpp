eba796b2cf4ea62ed7e734758cb34421b345ce05
NamSangKyu <nam2626@gmail.,com>
2021-12-02 20:11:18 +0900

    클래스

--- Step19/Step19/person.cpp
+++ Step19/Step19/person.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-struct Person {
+/*
+    접근제어자
+        private : 클래스 내부에서만 접근이 가능, 외부로부터 접근을 차단 
+        protected : 상속받은 클래스까지 접근이 가능
+        public : 외부로부터 접근을 허용, 해당 함수나 변수를 공개
+*/
+class Person {
+private:
+    //멤버 변수
     char name[30];
     int age;
-
+public:
+    //멤버 함수
     void Init(const char* n, int a) {
         //n은 name에 복사
         strcpy_s(name, sizeof(name), n);
@@ -20,7 +29,7 @@ struct Person {
 int main(void) {
     Person p;
     p.Init("홍길동", 20);
-    p.age = -20;
+    //p.age = -20; age가 private 영역에 있기 때문에 접근할 수 없다.
     p.PrintInfo();
     return 0;
 }
